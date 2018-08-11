b408226ba28c7e6b712359e73b81cee26c5e0474
青禾 <qinghe@qunhemail.com>
2018-08-11 10:27:47 +0800

    feat: 动态路由类型

--- 02/app.py
+++ 02/app.py
@@ -9,5 +9,9 @@ def index():
 def user(name):
     return '<h1>Hello %s!</h1>' % name
 
+@app.route('/user/<int:id>')
+def userid(id):
+    return '<h1>Hello %d!</h1>' % id
+
 if __name__ == '__main__':
     app.run(debug=True)
